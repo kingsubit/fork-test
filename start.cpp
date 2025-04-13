4125fd2af7f5681164bc452e07921a395d5479f9
[yunsoo] <[yunsoo2709@gmail.com]>
2025-04-14 00:56:42 +0900

    feat:생성자 구현 완료

--- start.cpp
+++ start.cpp
@@ -1,6 +1,30 @@
 #include<iostream>
 using namespace std;
 
+<<<<<<< HEAD
+=======
+class Node{
+    Node* next;
+    int data;
+    
+    friend class LinkedList;
+};
+
+class LinkedList{
+private:
+    Node* head;
+    Node* tail;
+    int n;
+public:
+    LinkedList();
+    int size();
+};
+LinkedList::LinkedList(){
+    head = tail = nullptr;
+    n = 0;
+}
+
+>>>>>>> 51e3161 (feat: 링크드리스트 구현)
 int main(){
     int a = 1, b = 2;
     cout << a + b;
